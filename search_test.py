dc47737a198d890743d4790e1c2bc6daeb8f286b
Aleena <63610006+a-leena@users.noreply.github.com>
2023-11-19 21:42:40 +0530

    added selenium testing code

---
+++ search_test.py
@@ -0,0 +1,30 @@
+import time
+from webdriver_manager.chrome import ChromeDriverManager 
+from selenium import webdriver 
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+
+# Set up the WebDriver (Chrome in this case)
+browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())) 
+browser.implicitly_wait(5)
+
+# Navigate to the home page
+browser.get('http://localhost:3000/')
+
+# Locate the search bar, enter a recipe name, and click search
+search_bar = browser.find_element(By.ID,'search_box')
+query = "Easy Baked Parmesan Chicken"
+search_bar.send_keys(query)
+search_button = browser.find_element(By.ID,'search_button')
+search_button.click()
+
+# Assertions to check if search results are displayed
+time.sleep(2)  # Wait for results to load
+page_heading = browser.find_element(By.CSS_SELECTOR,'.page-heading h1').text
+print(page_heading.lower())
+assert query.lower() in page_heading.lower()
+print("Correct Recipe opened")
+print("Test Passed!")
+
+# Close the browser
+browser.quit()
---
+++ suggestions_test.py
@@ -0,0 +1,31 @@
+import time
+from webdriver_manager.chrome import ChromeDriverManager 
+from selenium import webdriver 
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+
+# Set up the WebDriver (Chrome in this case)
+browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())) 
+browser.implicitly_wait(5)
+
+# Navigate to the home page
+browser.get('http://localhost:3000/')
+
+# Locate the search bar, enter a recipe name, and click search
+search_bar = browser.find_element(By.ID,'search_box')
+query = "Pasta"
+search_bar.send_keys(query)
+
+# Assertions to check if suggestions are displayed
+time.sleep(2)  # Wait for results to load
+suggestions = browser.find_elements(By.CLASS_NAME,'suggestionItem')
+# print(suggestions)
+assert len(suggestions) > 0
+print("Suggestions are shown")
+for suggestion in suggestions: title_element = suggestion.text
+assert query in title_element
+print("Suggestions contain the query")
+print("Tests Passed!")
+
+# Close the browser
+browser.quit()
